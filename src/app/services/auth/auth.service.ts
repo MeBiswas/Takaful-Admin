7a34234a613c202fd5de92b66429908306eadbe9
Abhipriyo Biswas <abhipriyo.biswas@etelligens.in>
2020-12-31 11:01:38 +0530

    minor change

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -6,11 +6,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
 
-  private REST_API_SERVER = "http://ilokensystem.ddns.net:28080/takaful/api/auth/login";
+  base_url = "http://ilokensystem.ddns.net:28080/takaful/api";
+
+  private REST_API_SERVER = "/auth/login";
 
   constructor(private http: HttpClient) { }
 
   public loginRequest(user) {
-    return this.http.post<any>(this.REST_API_SERVER, user);
+    return this.http.post<any>(this.base_url + this.REST_API_SERVER, user);
   }
 }
