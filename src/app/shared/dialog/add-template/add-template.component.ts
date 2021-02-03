4ac6a0bbc0329138a93cec27ffe3d0098677d435
Vaibhav <vaibhav.eteligens@gmail.com>
2021-02-03 14:04:16 +0530

    changes

--- src/app/shared/dialog/add-template/add-template.component.ts
+++ src/app/shared/dialog/add-template/add-template.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+// Toaster
+import { ToastrService } from 'ngx-toastr';
+// Interface
+import { Filter } from '../../../model/filter';
+// Spinner
+import { NgxSpinnerService } from 'ngx-spinner';
+// Form
+import { FormBuilder, Validators } from '@angular/forms';
+// Service
+import { AdminService } from '../../../services/admin/admin.service';
+// Regex Patterns
+const pattern = /^[a-zA-Z]*$/;
 
 @Component({
   selector: 'app-add-template',
   templateUrl: './add-template.component.html',
-  styleUrls: ['./add-template.component.css']
+  styleUrls: ['./add-template.component.css'],
 })
 export class AddTemplateComponent implements OnInit {
+  templateType: string = 'Email';
 
-  constructor() { }
+  AddTemplateForm = this._fb.group({
+    message: [''],
+    template: ['', Validators.required],
+    type: ['', [Validators.required, Validators.pattern(pattern)]],
+  });
 
-  ngOnInit(): void {
+  templateTypeList: Filter[] = [
+    { value: 'Email', option: 'Email' },
+    { value: 'SMS', option: 'SMS' },
+  ];
+  constructor(
+    private _fb: FormBuilder,
+    private _admin: AdminService,
+    private _toast: ToastrService,
+    private _spin: NgxSpinnerService
+  ) {}
+
+  ngOnInit(): void {}
+
+  // Form Field Getter
+  get template() {
+    return this.AddTemplateForm.get('template');
   }
-
+  get type() {
+    return this.AddTemplateForm.get('type');
+  }
 }
